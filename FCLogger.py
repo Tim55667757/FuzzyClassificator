3df5ee46a546f72b8fa71e60477a87b4a2fc7261
tgilmullin <TGilmullin@ptsecurity.ru>
2014-07-31 19:01:48 +0400

    FCLogger improvements

--- FCLogger.py
+++ FCLogger.py
@@ -49,7 +49,7 @@ import logging
 
 # initialize Main Parent Logger:
 FCLogger = logging.getLogger("FCLogger")
-formatString = "%(filename)-15s[Line:%(lineno)d]\t%(levelname)-10s[%(asctime)s]\t%(message)s"
+formatString = "%(filename)-25s[Line:%(lineno)d]\t%(levelname)-10s[%(asctime)s]\t%(message)s"
 formatter = logging.Formatter(formatString)
 sys.stderr = sys.stdout
 
@@ -83,11 +83,26 @@ def SetLevel(vLevel='ERROR'):
             FCLogger.parent.level = logging.DEBUG
 
 
+class LevelFilter(logging.Filter):
+    """
+    Class using to set up log level filtering.
+    """
+
+    def __init__(self, level):
+        super().__init__()
+        self.level = level
+
+    def filter(self, record):
+        return record.levelno >= self.level
+
+
 def EnableLogger(logFile, parentHandler=FCLogger, useFormat=formatter):
     """
     Adding new file logger.
     """
     logHandler = logging.FileHandler(logFile)
+    logHandler.level = logging.DEBUG
+    logHandler.addFilter(LevelFilter(logging.DEBUG))
 
     if useFormat:
         logHandler.setFormatter(useFormat)
